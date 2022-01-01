8050c534c8215e5e8e0018997d0a61e11261c6b4
Anthony Huerta <40744247+ajhuerta@users.noreply.github.com>
2022-01-01 13:42:26 -0600

    first commit
    Initial commit

---
+++ Buisness-Bots/Etsy-Bot/README.md
@@ -0,0 +1,2 @@
+The purpose of this bot is to be able to quickly collect data on any product that is available on Etsy. 
+Allows you to price products competatively fast.
---
+++ Buisness-Bots/Etsy-Bot/main.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import csv
+
+class EtsyScrapper:
+    def __init__(self):
+        # constructor variables
+        self.path_name = "/Users/anthonyh/Downloads/chromedriver"
+        self.base_url = "https://www.etsy.com/market/"
+        self.page_url = "?ref=pagination&page="
+        self.driver = webdriver.Chrome(self.path_name)
+
+    # This function will search for a specific item and find the corresponding 
+    def first_page_search(self, item):
+        # Checks to see if input is a string
+        if type(item) != str:
+            raise TypeError("Item must be a string format")
+
+        item = item.replace(" ", "_")
+
+        # Creates a new url using the item input by the user
+        new_url = self.base_url + item
+        self.driver.get(new_url)
+
+        # Collects the prices of every item as well as their name in two seperate lists
+        item_prices = list(self.driver.find_elements(By.CLASS_NAME, "currency-value"))
+        item_names = list(self.driver.find_elements(By.CLASS_NAME, "wt-mb-xs-0.wt-text-truncate.wt-text-caption.v2-listing-card__title"))
+
+        total = 0
+        item_dictionary = {}
+
+        # Loops through every item, removes any duplicates and organizes each item and it's price in a dictionary
+        for x in range(len(item_names)):
+            if item_names[x].text not in item_dictionary:
+                item_dictionary[item_names[x].text] = item_prices[x].text
+                total += float(item_prices[x].text.replace(",", ""))
+
+        # Creates the csv with all of the data that had been scrapped
+        with open('Etsy-Bot/output.csv', 'w') as output:
+            writer = csv.writer(output)
+
+            writer.writerow(["Category", "Price"])
+            for key, value in item_dictionary.items():
+                writer.writerow([key,value])
+
+            writer.writerow(["Average Price", total/len(item_prices)])
+        self.driver.quit()
+
+
+    def multi_page_search(self, item):
+
+        if type(item) != str:
+            raise TypeError("Item must be a string format")
+
+# Main function
+def main():
+    instance = EtsyScrapper()
+    instance.first_page_search("face oil")
+
+if __name__ == "__main__":
+    main()
